3f6cb55d8528c8515883f8e0fa6a075789467511
Belton  Menete <89537405+BeltonAlberto@users.noreply.github.com>
2023-09-04 18:18:10 +0200

    firt commit

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace ATMConsole_APP
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Just starting a simple, console ATM app");
+        }
+    }
+}
