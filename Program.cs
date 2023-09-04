cdfd82e83f331e4856b1cf811ea98754f7f9f97e
Belton Menete <bymenete@gmail.com>
2023-09-04 19:00:14 +0200

    add a very basic c# structure #3

--- Program.cs
+++ Program.cs
@@ -1,10 +1,41 @@
 ﻿namespace ATMConsole_APP
 {
-    internal class Program
+    public class CardHolder
     {
-        static void Main(string[] args)
+        private string cardNum;
+        private string firstName;
+        private string lastName;
+        private double balance;
+        private int pin;
+
+        public CardHolder(string cardNum, int pin, string firstName, string lastName, double balance)
         {
-            Console.WriteLine("Just starting a simple, console ATM app");
+            this.cardNum = cardNum;
+            this.firstName = firstName;
+            this.lastName = lastName;
+            this.balance = balance;
+            this.pin = pin;
         }
+
+        // Manual Getters
+
+        public string getNum() => cardNum;
+        public string getFirstName() => firstName;
+        public string getLastName() => lastName;
+        public double getBalance() => balance;
+        public int getPin() => pin;
+
+        // Manual setters
+        public void setNum(string newCardNum) => cardNum = newCardNum;
+        public void setFirstName(string newFirstName) => firstName = newFirstName;
+        public void setLastName(string newLastName) => lastName = newLastName;
+        public void setBalance(double newBalance) => balance = newBalance;
+        public void setPin(int newPin) => pin = newPin;
+
+        public static void Main(string[] args)
+        {
+
+        }
+
     }
 }
